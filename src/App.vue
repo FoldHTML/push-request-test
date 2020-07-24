b3fe58a000a62c580373d59780920795826c4048
hardanonymous <hard25670559@gmail.com>
2020-07-24 15:46:50 +0800

    CCC

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div id="nav">
       <h1>Hello vue.</h1>
       <h1>https://zh.wikipedia.org/wiki/%E7%BD%91%E7%BB%9C%E6%90%9C%E7%B4%A2%E5%BC%95%E6%93%8E</h1>
-      <h1>Hello vue.</h1>
       <h1>https://zh.wikipedia.org/wiki/%E7%BD%91%E7%BB%9C%E6%90%9C%E7%B4%A2%E5%BC%95%E6%93%8E</h1>
       <h1>https://zh.wikipedia.org/wiki/%E7%BD%91%E7%BB%9C%E6%90%9C%E7%B4%A2%E5%BC%95%E6%93%8E</h1>
       <router-link to="/">Home</router-link> |
